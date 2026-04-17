65ae42e4860e099cb2177b37e6d838d141d27e64
Elmo <113604229+elfilibusterismo@users.noreply.github.com>
2026-04-17 19:13:14 +0800

    build: Add JitPack and Google Maven repositories to `settings.gradle.kts`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.google.com") }
     }
 }
 
