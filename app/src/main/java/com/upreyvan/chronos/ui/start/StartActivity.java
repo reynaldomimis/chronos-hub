b3164c5df190804c1a409c4701749d690cd7387b
Elmo <113604229+elfilibusterismo@users.noreply.github.com>
2026-04-17 18:59:19 +0800

    feat: Create `StartActivity` and move landing screen logic from `MainActivity`

--- app/src/main/java/com/upreyvan/chronos/ui/start/StartActivity.java
+++ app/src/main/java/com/upreyvan/chronos/ui/start/StartActivity.java
@@ -1,4 +1,4 @@
-package com.upreyvan.chronos;
+package com.upreyvan.chronos.ui.start;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,8 +6,9 @@ import android.os.Bundle;
 import com.upreyvan.chronos.base.BaseActivity;
 import com.upreyvan.chronos.databinding.ActivityMainBinding;
 import com.upreyvan.chronos.ui.home.HomeActivity;
+import com.upreyvan.chronos.util.Utils;
 
-public class MainActivity extends BaseActivity {
+public class StartActivity extends BaseActivity {
 
     private ActivityMainBinding binding;
 
@@ -16,5 +17,10 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Utils.addSystemWindowInsetToPadding(binding.btnCreateAccount, true, false, true, true);
+        binding.btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        });
     }
 }
