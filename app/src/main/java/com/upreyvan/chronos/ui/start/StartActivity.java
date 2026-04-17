5f5c2a02b12387f8cfc03cd2956c64ff51622e2b
Elmo <113604229+elfilibusterismo@users.noreply.github.com>
2026-04-17 19:00:18 +0800

    fix: Update `StartActivity` to use `ActivityStartBinding` instead of `ActivityMainBinding`

--- app/src/main/java/com/upreyvan/chronos/ui/start/StartActivity.java
+++ app/src/main/java/com/upreyvan/chronos/ui/start/StartActivity.java
@@ -5,17 +5,18 @@ import android.os.Bundle;
 
 import com.upreyvan.chronos.base.BaseActivity;
 import com.upreyvan.chronos.databinding.ActivityMainBinding;
+import com.upreyvan.chronos.databinding.ActivityStartBinding;
 import com.upreyvan.chronos.ui.home.HomeActivity;
 import com.upreyvan.chronos.util.Utils;
 
 public class StartActivity extends BaseActivity {
 
-    private ActivityMainBinding binding;
+    private ActivityStartBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityStartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         Utils.addSystemWindowInsetToPadding(binding.btnCreateAccount, true, false, true, true);
         binding.btnLogin.setOnClickListener(v -> {
