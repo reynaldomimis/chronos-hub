12dc7408bc5729ef62880c5b7f30662cadd256af
Elmo <113604229+elfilibusterismo@users.noreply.github.com>
2026-04-17 19:13:06 +0800

    refactor: Remove unused UI activities and relocate StartActivity

--- app/src/main/java/com/upreyvan/chronos/activities/StartActivity.java
+++ app/src/main/java/com/upreyvan/chronos/activities/StartActivity.java
@@ -1,12 +1,11 @@
-package com.upreyvan.chronos.ui.start;
+package com.upreyvan.chronos.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.upreyvan.chronos.base.BaseActivity;
-import com.upreyvan.chronos.databinding.ActivityMainBinding;
 import com.upreyvan.chronos.databinding.ActivityStartBinding;
-import com.upreyvan.chronos.ui.home.HomeActivity;
+import com.upreyvan.chronos.MainActivity;
 import com.upreyvan.chronos.util.Utils;
 
 public class StartActivity extends BaseActivity {
@@ -20,7 +19,7 @@ public class StartActivity extends BaseActivity {
         setContentView(binding.getRoot());
         Utils.addSystemWindowInsetToMargin(binding.btnCreateAccount, true, false, true, true);
         binding.btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(this, HomeActivity.class);
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
     }
