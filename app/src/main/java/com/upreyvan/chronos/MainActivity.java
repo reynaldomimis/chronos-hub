34d34b24684fb77d1c1cc285041fc17c1cd7c4ae
Elmo <113604229+elfilibusterismo@users.noreply.github.com>
2026-04-17 18:49:02 +0800

    refactor: Implement View Binding in `MainActivity`

--- app/src/main/java/com/upreyvan/chronos/MainActivity.java
+++ app/src/main/java/com/upreyvan/chronos/MainActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.upreyvan.chronos.base.BaseActivity;
+import com.upreyvan.chronos.databinding.ActivityMainBinding;
 import com.upreyvan.chronos.ui.home.HomeActivity;
 
 public class MainActivity extends BaseActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.btnLogin).setOnClickListener(v -> {
-            startActivity(new Intent(this, HomeActivity.class));
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
         });
     }
 }
